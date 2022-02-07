4da9e15ab6f27d6c88f22038246394bdead3bf00
Pedro Lima <pedrolc05@gmail.com>
2022-02-07 07:18:32 -0400

    Second test on astyle with main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
 #include "Arduino.h"
 
-void setup(){}
+void setup()
+{
+  Serial.begin (115200);
+  Serial.println ("Initial program");
+}
+
+void loop()
+{
+  Serial.println ("hola mundo");
+  delay (1000);
+}
