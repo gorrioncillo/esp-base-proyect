63e29b40ce0e069d639ed8695d2235a4c2a54c1c
Pedro Lima <pedrolc05@gmail.com>
2022-02-07 16:06:33 -0400

    6th test on astyle with main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 
 void setup()
 {
-Serial.begin (115200);
- Serial.println ("Initial program");
+  Serial.begin (115200);
+  Serial.println ("Initial program");
 }
 
 void loop()
 {
-Serial.println ("hola mundo");
-delay (1000);
+  Serial.println ("hola mundo");
+  delay (1000);
 }
